18cc0d8fa31e20fac970f693f7919a13b692e9cf
Jouramie <Jouramie@hotmail.com>
2015-02-11 17:32:13 -0500

    Interface Corps + Classe Planète

--- src/modele/Corps.java
+++ src/modele/Corps.java
@@ -1,17 +1,23 @@
 package modele;
 
+import javafx.beans.property.DoubleProperty;
 import utils.Vecteur;
 
 public interface Corps
 {	
 	double getMasse();
-	void setMasse(double masse);
+	void setMasse(double pMasse);
 	Vecteur getForceExt();
 	
-	void setPosition();
+	void setPosition(Vecteur pPosition);
 	Vecteur getPosition();
+	double getPositionX();
+	double getPositionY();
+	DoubleProperty getPositionXProperty();
+	DoubleProperty getPositionYProperty();
 	
 	boolean isStatique();
-	void setVitesse();
+	void setStatique(boolean pStatique);
 	Vecteur getVitesse();
+	void setVitesse(Vecteur pVitesse);
 }
--- src/objets/Planete.java
+++ src/objets/Planete.java
@@ -1,5 +1,7 @@
 package objets;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Node;
 import modele.Corps;
 import utils.Vecteur;
@@ -9,51 +11,111 @@ public class Planete implements Corps, Dessinable
 {
 	private double masse;
 	private Vecteur position;
-	private boolean tatique;
-	
-	
-	public Planete()
+	private DoubleProperty positionX;
+	private DoubleProperty positionY;
+	private boolean statique;
+	private Vecteur vitesse;
+
+	public Planete(double pMasse, Vecteur pPosition, boolean pStatique,
+			Vecteur pVitesse)
 	{
-		
-	}
-	
-	public double getMasse()
-	{
-		return 0;
+		masse = pMasse;
+		if (pPosition == null)
+			position = new Vecteur();
+		else
+			position = pPosition;
+		statique = pStatique;
+		if (pVitesse == null)
+			vitesse = new Vecteur();
+		else
+			vitesse = pVitesse;
 	}
 
-	public void setPosition()
+	public Planete(double pMasse, double pPositionX, double pPositionY,
+			boolean pStatique, Vecteur pVitesse)
 	{
-		
+		masse = pMasse;
+		positionX = new SimpleDoubleProperty(pPositionX);
+		positionY = new SimpleDoubleProperty(pPositionY);
+		statique = pStatique;
+		if (pVitesse == null)
+			vitesse = new Vecteur();
+		else
+			vitesse = pVitesse;
 	}
+
+	public void setMasse(double pMasse)
+	{
+		masse = pMasse;
+	}
+
+	public double getPositionX()
+	{
+		return positionX.get();
+	}
+
+	public double getPositionY()
+	{
+		return positionY.get();
+	}
+
+	public DoubleProperty getPositionXProperty()
+	{
+		return positionX;
+	}
+
+	public DoubleProperty getPositionYProperty()
+	{
+		return positionY;
+	}
+
+	public void setPosition(Vecteur pPosition)
+	{
+		position = pPosition;
+	}
+
+	public void setStatique(boolean pStatique)
+	{
+		statique = pStatique;
+	}
+
+	public void setVitesse(Vecteur pVitesse)
+	{
+		vitesse = pVitesse;
+	}
+
+	public Node getNoeud()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public double getMasse()
+	{
+		return masse;
+	}
+
+
+	public Vecteur getForceExt()
+	{
+		// TODO
+		return null;
+	}
+
 
 	public Vecteur getPosition()
 	{
-		return null;
+		return position;
 	}
 	
 	public boolean isStatique()
 	{
-		return true;
-	}
-
-	public void setVitesse()
-	{
-		
+		return statique;
 	}
 
 	public Vecteur getVitesse()
 	{
-		return null;
-	}
-
-	public Vecteur getForceExt()
-	{
-		return null;
-	}
-	
-	public Node getNoeud()
-	{
-		return null;
+		return vitesse;
 	}
 }
