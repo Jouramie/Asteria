528dc99b10c185c96ee33e3dda9f511629d63cf5
Jouramie <Jouramie@hotmail.com>
2015-02-18 18:07:29 -0500

    Création de la classe PlaneteTest

---
+++ src/tests/PlaneteTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlaneteTest
+{
+	
+	@Test
+	public void testPlaneteDoubleVecteur()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlaneteDoubleDoubleDouble()
+	{
+		fail("Not yet implemented");
+	}
+	
+}
