a080d1ed312b42ff35c748d9e6ae053f3ed7d834
Jowpac <jo_exo@hotmail.com>
2015-02-18 16:01:43 -0500

    Petites erreurs

--- src/controleur/ContJeu.java
+++ src/controleur/ContJeu.java
@@ -19,10 +19,10 @@ public class ContJeu implements Controleur
 	
 	public void initialiser()
 	{
-		Planete p1 = new Planete(100000, new Vecteur(400, 400), new Vecteur(0, 0));
+		Planete p1 = new Planete(100000, new Vecteur(400, 400));
 		ContPrincipal.getInstance().ajouterCorps(p1);
 		
-		Vaisseau v = new Vaisseau(100, new Vecteur(0, 0), 100, 100, new Vecteur(10, 10), false, new Vecteur(50, 0));
+		Vaisseau v = new Vaisseau(100, new Vecteur(0, 0), 100, 100, new Vecteur(10, 10), new Vecteur(50, 0));
 		ContPrincipal.getInstance().ajouterCorps(v);
 		
 		ContPrincipal.getInstance().afficherVue(new VueJeu());
