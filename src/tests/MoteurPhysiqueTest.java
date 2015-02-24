9fe00bfed9b5d5fd928c7879374487c6c724b363
1335647 <simon.pierre@hotmail.com>
2015-02-24 15:16:22 -0500

    Écriture du test de MoteurPhysique.

--- src/tests/MoteurPhysiqueTest.java
+++ src/tests/MoteurPhysiqueTest.java
@@ -1,17 +1,13 @@
 package tests;
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import modele.Corps;
 import modele.MoteurPhysique;
 import objets.Planete;
 import objets.Vaisseau;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import utils.Vecteur;
 /**
  * Classe de tests pour MoteurPhysique.
@@ -28,15 +24,22 @@ public class MoteurPhysiqueTest
 	{
 		moteur = new MoteurPhysique();
 		corps = new ArrayList<>();
-		vaisseau = new Vaisseau(0, new Vecteur(1, 0), 70000, 0, new Vecteur(0, 120), new Vecteur(10, 0));
+		vaisseau = new Vaisseau(0, new Vecteur(1, 0), 100, 0, new Vecteur(0, 120), new Vecteur(10, 0));
 		corps.add(vaisseau);
-		corps.add(new Planete(15707.96327, null));
+		corps.add(new Planete(15707.96327, new Vecteur()));
+		corps.add(null);
 	}
 	
 	@Test
 	public void testUpdate()
 	{
 		moteur.update(corps, 1);
+		assertEquals(vaisseau.getPositionX(), 10, 0.0001);
+		moteur.update(corps, 0);
+		assertEquals(vaisseau.getPositionX(), 10, 0.0001);
+		moteur.update(null, 1);
+		assertEquals(vaisseau.getPositionX(), 10, 0.0001);
+		moteur.update(null, 0);
 		assertEquals(vaisseau.getPositionX(), 10, 0.0001);
 	}
 }
