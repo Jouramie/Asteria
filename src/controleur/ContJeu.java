e6579d5302ea30647757f1f6b6f27bf103bb0ca4
Jouramie <Jouramie@hotmail.com>
2015-03-09 11:35:39 -0400

    Ajout de l'enum texture

--- src/controleur/ContJeu.java
+++ src/controleur/ContJeu.java
@@ -3,9 +3,9 @@ package controleur;
 import javafx.fxml.FXML;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import objets.Planete;
+import objets.Planete.Texture;
 import objets.VaisseauJoueur;
 import utils.Vecteur;
 import vue.Camera;
@@ -17,137 +17,118 @@ import vue.VueJeu;
  * @author EquBolduc
  * @version 1.0
  */
-public class ContJeu implements Controleur
-{
+public class ContJeu implements Controleur {
 	public static final double VITESSE_ZOOM = 0.005;
-	
+
 	@FXML
 	private VBox menuPause;
-	
+
 	private VueJeu vue;
 	private VaisseauJoueur vaisseauJoueur;
 	private boolean aPressed;
 	private boolean dPressed;
 	private boolean wPressed;
-	
+
 	/**
 	 * Constructeur du contrôleur.
 	 */
-	public ContJeu()
-	{
+	public ContJeu() {
 		vue = new VueJeu();
 	}
-	
+
 	/**
 	 * Initialise les éléments du jeu et affiche la vue. Démarre l'horloge du
 	 * contrôleur principal.
 	 */
-	public void initialiser()
-	{
+	public void initialiser() {
 		ContPrincipal.getInstance().demarrerHorloge();
-		
+
 		Planete p1 = new Planete(5e15, new Vecteur(400, 400),
-				"/res/planete1.png");
+				Texture.RAYEE_ROUGE);
 		ContPrincipal.getInstance().ajouterCorps(p1);
-		
-		Planete p2 = new Planete(5e15, new Vecteur(600, 0), "/res/planete1.png");
+
+		Planete p2 = new Planete(5e15, new Vecteur(600, 0), Texture.RAYEE_ROUGE);
 		ContPrincipal.getInstance().ajouterCorps(p2);
-		
+
 		Planete p3 = new Planete(1e15, new Vecteur(600, 600),
-				"/res/planete1.png");
+				Texture.RAYEE_ROUGE);
 		ContPrincipal.getInstance().ajouterCorps(p3);
-		
-		Planete p4 = new Planete(1e15, new Vecteur(0, 600), "/res/planete1.png");
+
+		Planete p4 = new Planete(1e15, new Vecteur(0, 600), Texture.RAYEE_ROUGE);
 		ContPrincipal.getInstance().ajouterCorps(p4);
-		
+
 		vaisseauJoueur = new VaisseauJoueur(2167.27e2, new Vecteur(0, 0), 16e3,
 				100, new Vecteur(10, 10), new Vecteur(10, 10));
 		ContPrincipal.getInstance().ajouterCorps(vaisseauJoueur);
-		
+
 		ContPrincipal.getInstance().afficherVue(vue, true);
 	}
-	
-	public void afficherMenuPause()
-	{
+
+	public void afficherMenuPause() {
 		ContPrincipal.getInstance().arreterHorloge();
 		menuPause.setVisible(true);
 		menuPause.toFront();
 	}
-	
-	public void cacherMenuPause()
-	{
+
+	public void cacherMenuPause() {
 		ContPrincipal.getInstance().demarrerHorloge();
 		menuPause.setVisible(false);
 	}
-	
+
 	@FXML
-	public void retour()
-	{
+	public void retour() {
 		ContPrincipal.getInstance().viderCorps();
 		ContPrincipal.getInstance().selectionnerControleur(new ContMenu());
 	}
-	
+
 	@FXML
-	public void retourjeu()
-	{
+	public void retourjeu() {
 		cacherMenuPause();
 	}
-	
+
 	@FXML
-	public void zoom(ScrollEvent e)
-	{
+	public void zoom(ScrollEvent e) {
 		Camera cam = vue.getCamera();
-		
+
 		double delta = e.getDeltaY();
-		
-		if (delta > 0)
-		{
+
+		if (delta > 0) {
 			cam.zoomer(cam.getFacteur() + delta * VITESSE_ZOOM);
 		}
-		
-		else
-		{
+
+		else {
 			cam.zoomer(cam.getFacteur() + delta * VITESSE_ZOOM);
 		}
 	}
-	
+
 	@FXML
-	public void keyPressed(KeyEvent e)
-	{
-		switch (e.getCode())
-		{
-		case ESCAPE:
-		{
-			if (!menuPause.isVisible())
-			{
+	public void keyPressed(KeyEvent e) {
+		switch (e.getCode()) {
+		case ESCAPE: {
+			if (!menuPause.isVisible()) {
 				afficherMenuPause();
-			}
-			else
-			{
+			} else {
 				cacherMenuPause();
 			}
 			break;
 		}
 		case LEFT:
 		case A:
-			if (!aPressed)
-			{
+			if (!aPressed) {
 				vaisseauJoueur.tournerGauche();
 				aPressed = true;
 			}
 			break;
 		case RIGHT:
 		case D:
-			if (!dPressed)
-			{
+			if (!dPressed) {
 				vaisseauJoueur.tournerDroite();
 				dPressed = true;
 			}
 			break;
 		case UP:
 		case W:
-			if (!wPressed)
-			{
+			if (!wPressed) {
 				vaisseauJoueur.avancer();
 				wPressed = true;
 			}
@@ -156,32 +137,27 @@ public class ContJeu implements Controleur
 			break;
 		}
 	}
-	
+
 	@FXML
-	public void keyReleased(KeyEvent e)
-	{
-		switch (e.getCode())
-		{
+	public void keyReleased(KeyEvent e) {
+		switch (e.getCode()) {
 		case LEFT:
 		case A:
-			if (aPressed)
-			{
+			if (aPressed) {
 				vaisseauJoueur.tournerGauche();
 				aPressed = false;
 			}
 			break;
 		case RIGHT:
 		case D:
-			if (dPressed)
-			{
+			if (dPressed) {
 				vaisseauJoueur.tournerDroite();
 				dPressed = false;
 			}
 			break;
 		case UP:
 		case W:
-			if (wPressed)
-			{
+			if (wPressed) {
 				vaisseauJoueur.avancer();
 				wPressed = false;
 			}
@@ -190,12 +166,11 @@ public class ContJeu implements Controleur
 			break;
 		}
 	}
-	
+
 	/**
 	 * Met à jour le vaisseau.
 	 */
-	public void update(double dt)
-	{
+	public void update(double dt) {
 		Camera camera = vue.getCamera();
 		camera.deplacer(vaisseauJoueur.getPositionX(),
 				vaisseauJoueur.getPositionY());
--- src/objets/Planete.java
+++ src/objets/Planete.java
@@ -13,61 +13,80 @@ import utils.Vecteur;
 
 /**
  * Classe représentant une planète.
+ * 
  * @author EquBolduc
  * @version 1.0
  */
-public class Planete extends ObjetSpatial
-{
+public class Planete extends ObjetSpatial {
+
+	public enum Texture {
+		RAYEE_ROUGE("/res/planete1.png");
+
+		private final String texture;
+
+		Texture(String pTexture) {
+			texture = pTexture;
+		}
+
+		public String getTexture() {
+			return texture;
+		}
+	}
+
 	private Image texture;
 
 	/**
 	 * Constructeur de planète, prend un vecteur pour la position
 	 * 
-	 * @param pMasse la masse de la planète
-	 * @param pPosition la position de la planète
+	 * @param pMasse
+	 *            la masse de la planète
+	 * @param pPosition
+	 *            la position de la planète
 	 */
-	public Planete(double pMasse, Vecteur pPosition, String tex)
-	{
+	public Planete(double pMasse, Vecteur pPosition, Texture pTexture) {
 		super(pMasse, pPosition, true, new Vecteur());
-		texture = new Image(tex);
+		texture = new Image(pTexture.getTexture());
 	}
 
 	/**
 	 * Constructeur de planète, prend des doubles pour la position
 	 * 
-	 * @param pMasse la masse de la planète
-	 * @param pPositionX la positionX de la planète
-	 * @param pPositionY la positionY de la planète
+	 * @param pMasse
+	 *            la masse de la planète
+	 * @param pPositionX
+	 *            la positionX de la planète
+	 * @param pPositionY
+	 *            la positionY de la planète
 	 */
-	public Planete(double pMasse, double pPositionX, double pPositionY, String tex)
-	{
+	public Planete(double pMasse, double pPositionX, double pPositionY,
+			String tex) {
 		super(pMasse, pPositionX, pPositionY, true, new Vecteur());
 		texture = new Image(tex);
 	}
 
-	public int getRayonCollision()
-	{
+	public int getRayonCollision() {
 		return 100;
 	}
 
-	public Node getNoeud()
-	{
+	public Node getNoeud() {
 		double rayon = getRayonCollision();
-		
+
 		Circle cercle = new Circle(rayon + 30);
-		RadialGradient grad = new RadialGradient(0, 0, 0, 0, rayon + 32, false, CycleMethod.REPEAT, new Stop(0.7, Color.ORANGE), new Stop(1, Color.TRANSPARENT));
+		RadialGradient grad = new RadialGradient(0, 0, 0, 0, rayon + 32, false,
+				CycleMethod.REPEAT, new Stop(0.7, Color.ORANGE), new Stop(1,
+						Color.TRANSPARENT));
 		cercle.setFill(grad);
-		
+
 		ImageView image = new ImageView(texture);
 		image.setFitWidth(rayon * 2);
 		image.setFitHeight(rayon * 2);
 		image.setTranslateX(-rayon);
 		image.setTranslateY(-rayon);
-		
+
 		Group group = new Group();
 		group.getChildren().add(cercle);
 		group.getChildren().add(image);
-		
+
 		return group;
 	}
 }
