e3c69935792fdbc48fca6deb34d1fb3b773e5181
Jouramie <Jouramie@hotmail.com>
2015-03-25 17:25:31 -0400

    Contrôleur niveau

--- src/controleur/ContNiveau.java
+++ src/controleur/ContNiveau.java
@@ -2,19 +2,21 @@ package controleur;
 
 import java.util.List;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import modele.Corps;
-import objets.ObjetSpatial;
 import objets.Planete;
 import objets.Planete.Texture;
 import objets.Vaisseau;
@@ -43,11 +45,25 @@ public class ContNiveau implements Controleur
 	@FXML
 	private Button erase;
 	@FXML
-	private ChoiceBox<String> choice;
+	private ChoiceBox<String> choiceBoxCorps;
 	@FXML
-	private ColorPicker color;
+	private ChoiceBox<Image> choiceBoxTexture;
 	@FXML
-	private Slider masse;
+	private TextField textFieldMasse;
+	@FXML
+	private TextField textFieldPositionX;
+	@FXML
+	private TextField textFieldPositionY;
+	@FXML
+	private TextField textFieldRayon;
+	@FXML
+	private TextField textFieldVitesseDepart;
+	@FXML
+	private VBox vBoxMenuPlanete;
+	@FXML
+	private VBox vBoxMenuVaisseau;
+	@FXML
+	private VBox vBoxMenu;
 	
 	private VueNiveau vue;
 	
@@ -55,6 +71,7 @@ public class ContNiveau implements Controleur
 	private boolean rightPressed;
 	private boolean upPressed;
 	private boolean downPressed;
+	private Corps corpsSelect;
 	
 	/**
 	 * Constructeur du contrôleur.
@@ -74,7 +91,25 @@ public class ContNiveau implements Controleur
 	public void initialiser()
 	{
 		ContPrincipal.getInstance().afficherVue(vue, true);
-		choice.getItems().addAll("Vaisseau", "Planète", "Drapeau");
+		choiceBoxCorps.getItems().addAll("Vaisseau", "Planète", "Drapeau");
+		textFieldRayon.setOnAction(new EventHandler<ActionEvent>()
+		{
+			public void handle(ActionEvent e)
+			{
+				try
+				{
+					((Planete) corpsSelect).setRayon((Double
+							.valueOf(textFieldRayon.getText())));
+					System.out.println(corpsSelect.getRayon());
+				}
+				catch (Exception ex)
+				{
+					ex.printStackTrace();
+				}
+				
+			}
+			
+		});
 	}
 	
 	/**
@@ -83,7 +118,7 @@ public class ContNiveau implements Controleur
 	public void update(double dt)
 	{
 		Camera cam = vue.getCamera();
-		if(leftPressed)
+		if (leftPressed)
 		{
 			double x = cam.getDeplacement().getX();
 			double y = cam.getDeplacement().getY();
@@ -93,7 +128,7 @@ public class ContNiveau implements Controleur
 			cam.deplacer(x, y);
 		}
 		
-		else if(rightPressed)
+		else if (rightPressed)
 		{
 			double x = cam.getDeplacement().getX();
 			double y = cam.getDeplacement().getY();
@@ -103,7 +138,7 @@ public class ContNiveau implements Controleur
 			cam.deplacer(x, y);
 		}
 		
-		if(upPressed)
+		if (upPressed)
 		{
 			double x = cam.getDeplacement().getX();
 			double y = cam.getDeplacement().getY();
@@ -113,7 +148,7 @@ public class ContNiveau implements Controleur
 			cam.deplacer(x, y);
 		}
 		
-		else if(downPressed)
+		else if (downPressed)
 		{
 			double x = cam.getDeplacement().getX();
 			double y = cam.getDeplacement().getY();
@@ -157,43 +192,83 @@ public class ContNiveau implements Controleur
 	 * méthode qui gère l'ajout d'objets dans la construction.
 	 */
 	@FXML
-	public void mouseClicked(MouseEvent e)
+	public void mouseClicked(MouseEvent event)
 	{
 		ContPrincipal.getInstance().arreterHorloge();
-		Point2D point = pane.sceneToLocal(e.getSceneX(), e.getSceneY());
-		ObjetSpatial u = null;
+		Point2D point = pane.sceneToLocal(event.getSceneX(), event.getSceneY());
 		Camera cam = vue.getCamera();
 		Vecteur pos = cam
 				.localToGlobal(new Vecteur(point.getX(), point.getY()));
-		
-		if (e.getButton() == MouseButton.PRIMARY)
+		switch (event.getButton())
 		{
-			if (choice.getValue() == "Vaisseau")
+		case PRIMARY:
+			mouseClickedPrimary(event, pos);
+			break;
+		case SECONDARY:
+			mouseClickedSecondary(event, pos);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private void mouseClickedPrimary(MouseEvent event, Vecteur pos)
+	{
+		boolean toucheCorps = false;
+		corpsSelect = null;
+		for (Corps c : ContPrincipal.getInstance().getCorps())
+		{
+			if (Math.abs(c.getPosition().getX() - pos.getX()) < c.getRayon()
+					&& Math.abs(c.getPosition().getY() - pos.getY()) < c
+							.getRayon())
 			{
-				u = new Vaisseau(0, 0, 0, pos.getX(), pos.getY(), null);
+				toucheCorps = true;
+				corpsSelect = c;
+				break;
 			}
-			else if (choice.getValue() == "Planète")
+		}
+		if (!toucheCorps && choiceBoxCorps.getValue() != null)
+		{
+			switch (choiceBoxCorps.getValue())
 			{
-				u = new Planete(6e15, pos.getX(), pos.getY(), 100);
-				((Planete) u).setTexture(Texture.RAYEE_ROUGE);
+			case "Planète":
+				corpsSelect = new Planete(6e15, pos.getX(), pos.getY(), 100);
+				((Planete) corpsSelect).setTexture(Texture.RAYEE_ROUGE);
+				break;
+			case "Vaisseau":
+				corpsSelect = new Vaisseau(0, 1000, 0, pos.getX(), pos.getY(),
+						null);
+				break;
 			}
-			ContPrincipal.getInstance().ajouterCorps(u);
+			ContPrincipal.getInstance().ajouterCorps(corpsSelect);
 			vue.initialiserCorps();
 			pane.requestFocus();
+			
 		}
-		else if (e.getButton() == MouseButton.SECONDARY)
+		selectionnerCorps();
+		
+	}
+	
+	private void selectionnerCorps()
+	{
+		vBoxMenu.setVisible(true);
+		if (corpsSelect.getClass().equals(Planete.class))
 		{
-			for (Corps c : ContPrincipal.getInstance().getCorps())
-			{
-				if (Math.abs(c.getPosition().getX() - pos.getX()) < c
-						.getRayon()
-						&& Math.abs(c.getPosition().getY() - pos.getY()) < c
-								.getRayon())
-				{
-					System.out.println(c.getMasse());
-				}
-			}
+			vBoxMenuPlanete.setVisible(true);
+			vBoxMenuVaisseau.setVisible(false);
+			textFieldRayon.setText("" + corpsSelect.getRayon());
+			
 		}
+		else if (corpsSelect.getClass().equals(Vaisseau.class))
+		{
+			vBoxMenuPlanete.setVisible(false);
+			vBoxMenuVaisseau.setVisible(true);
+		}
+	}
+	
+	private void mouseClickedSecondary(MouseEvent event, Vecteur pos)
+	{
+		// TODO ???
 	}
 	
 	@FXML
@@ -201,29 +276,34 @@ public class ContNiveau implements Controleur
 	{
 		switch (e.getCode())
 		{
-			case A:
-			{
-				leftPressed = true;
-				break;
-			}
-			case D:
-			{
-				rightPressed = true;
-				break;
-			}
-			case W:
-			{
-				upPressed = true;
-				break;
-			}
-			case S:
-			{
-				downPressed = true;
-				break;
-			}
-			
-			default:
-				break;
+		case A:
+		{
+			leftPressed = true;
+			break;
+		}
+		case D:
+		{
+			rightPressed = true;
+			break;
+		}
+		case W:
+		{
+			upPressed = true;
+			break;
+		}
+		case S:
+		{
+			downPressed = true;
+			break;
+		}
+		case ESCAPE:
+		{
+			retour();
+			break;
+		}
+		
+		default:
+			break;
 		}
 	}
 	
@@ -232,29 +312,29 @@ public class ContNiveau implements Controleur
 	{
 		switch (e.getCode())
 		{
-			case A:
-			{
-				leftPressed = false;
-				break;
-			}
-			case D:
-			{
-				rightPressed = false;
-				break;
-			}
-			case W:
-			{
-				upPressed = false;
-				break;
-			}
-			case S:
-			{
-				downPressed = false;
-				break;
-			}
-			
-			default:
-				break;
+		case A:
+		{
+			leftPressed = false;
+			break;
+		}
+		case D:
+		{
+			rightPressed = false;
+			break;
+		}
+		case W:
+		{
+			upPressed = false;
+			break;
+		}
+		case S:
+		{
+			downPressed = false;
+			break;
+		}
+		
+		default:
+			break;
 		}
 	}
 	
